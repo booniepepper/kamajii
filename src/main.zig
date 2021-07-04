e32a03b61afb6dd531a4038f8be2ba5ea97f2349
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-04 02:20:14 -0700

    write to a file

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
+const fs = std.fs;
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const file = try fs.cwd().createFile(
+        "temp.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    try file.writeAll("All your codebase are belong to us.\n\n");
 }
