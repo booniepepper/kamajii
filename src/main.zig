5530340e3d92b718c06e555b81426a181d1fa9f2
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-07 12:13:51 -0700

    Try some input/output

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,21 @@
 const std = @import("std");
+const stdin = std.io.getStdIn();
+const stdout = std.io.getStdOut();
 const fs = std.fs;
+const bufSize = 128;
 
-pub fn main() anyerror!void {
-    try writeFile("temp1.txt", "testing");
-    try writeFile("temp2.txt", "testing\n");
-    try writeFile("temp3.txt", "testing\n\n");
+pub fn main() !void {
+    try stdout.writeAll("Enter your name: ");
+
+    // We need a buffer that can stay in scope longer than the result of getLine
+    var buffer: [bufSize]u8 = undefined;
+    const line = getLine(&buffer);
+
+    try stdout.writer().print("Greetings {s}.\n", .{line});
+
+    //try writeFile("temp1.txt", "testing");
+    //try writeFile("temp2.txt", "testing\n");
+    //try writeFile("temp3.txt", "testing\n\n");
 }
 
 fn writeFile(filename: []const u8, contents: []const u8) anyerror!void {
@@ -15,4 +26,12 @@ fn writeFile(filename: []const u8, contents: []const u8) anyerror!void {
     defer file.close();
 
     try file.writeAll(contents);
+}
+
+fn getLine(buffer: []u8) ![]const u8 {
+    const line = try stdin.reader().readUntilDelimiterOrEof(
+        buffer,
+        '\n',
+    );
+    return line orelse "";
 }
