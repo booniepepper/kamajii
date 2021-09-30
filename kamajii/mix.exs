d69b7e2edb621ba292b67b26bedf9625ecf46ce8
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-09-30 00:27:58 -0700

    Start a Raxx/Ace project.
    mix archive.install hex raxx_kit

---
+++ kamajii/mix.exs
@@ -0,0 +1,35 @@
+defmodule Kamajii.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :kamajii,
+      version: "0.1.0",
+      elixir: "~> 1.12.3",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger], mod: {Kamajii.Application, []}]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:ace, "~> 0.18.6"},
+      {:raxx_logger, "~> 0.2.2"},
+      {:jason, "~> 1.0"}
+    ]
+  end
+
+  defp aliases() do
+    []
+  end
+end
