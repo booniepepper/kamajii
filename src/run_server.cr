884d5670946b0ca4bb57de9d80d5b6a4fa6b04a2
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-10-05 03:52:20 -0700

    add docs and a stub push api

--- src/run_server.cr
+++ src/run_server.cr
@@ -1,8 +1,17 @@
+# Kemal is a microservice framework. (Like Sinatra/Flask)
+# Docs: https://kemalcr.com
 require "kemal"
 
 get "/" do |env|
   env.response.content_type = "application/json"
   {message: "greetings"}.to_json
+end
+
+post "/push/:stack" do |env|
+  stack = env.params.url["stack"]
+  item = env.params.body.inspect
+  env.response.content_type = "application/json"
+  {stack: stack, item: item}.to_json
 end
 
 error 404 do |env|
