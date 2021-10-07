09244b22f82ad34c5ebdf9c7dee967c485cb0031
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-10-07 02:22:34 -0700

    add push/pop

---
+++ lib/kamajii/src/kamajii.cr
@@ -0,0 +1,58 @@
+require "file_utils"
+
+# TODO: Write documentation for `Kamajii`
+module Kamajii
+  extend self
+
+  def push(stack : Path | String, contents : String)
+    dir = stackdir stack
+    item = File.join dir, "item"
+    nexxt = File.join dir, "next"
+
+    if File.exists? item
+      if File.exists? nexxt
+        temp_nexxt = File.join dir, "temp_next"
+
+        FileUtils.mv nexxt, temp_nexxt
+        Dir.mkdir nexxt
+        FileUtils.mv temp_nexxt, File.join(nexxt, "next")
+        FileUtils.mv item, File.join(nexxt, "item")
+      else
+        Dir.mkdir nexxt
+        FileUtils.mv item, File.join(nexxt, "item")
+      end
+    end
+
+    File.write item, contents
+  end
+
+  def pop(stack : Path | String) : String?
+    dir = stackdir stack
+    item = File.join dir, "item"
+
+    content = nil
+    if File.exists? item
+      content = File.read item
+      FileUtils.rm item
+    end
+
+    nexxt = File.join dir, "next"
+    if File.exists? nexxt
+      temp_nexxt = File.join dir, "temp_next"
+      FileUtils.mv nexxt, temp_nexxt
+      Dir.each_child temp_nexxt do |child|
+        FileUtils.mv File.join(temp_nexxt, child), File.join(dir, child)
+      end
+      FileUtils.rmdir temp_nexxt
+    end
+
+    content
+  end
+
+  def stackdir(stack : Path | String) : String
+    home = Path.home
+    path = File.join(home, ".kamajii", stack)
+    Dir.mkdir_p(path)
+    path
+  end
+end
--- src/run_server.cr
+++ src/run_server.cr
@@ -1,17 +1,38 @@
 # Kemal is a microservice framework. (Like Sinatra/Flask)
 # Docs: https://kemalcr.com
 require "kemal"
+require "kamajii"
 
 get "/" do |env|
+  content = env.request.body
+
+  request = content ? content.gets_to_end : "{}"
+
   env.response.content_type = "application/json"
-  {message: "greetings"}.to_json
+  {message: "greetings", request: request}.to_json
 end
 
 post "/push/:stack" do |env|
   stack = env.params.url["stack"]
-  item = env.params.body.inspect
-  env.response.content_type = "application/json"
-  {stack: stack, item: item}.to_json
+  content = env.request.body
+
+  item = content ? content.gets_to_end : ""
+
+  Kamajii.push stack, item
+
+  env.response.content_type = "text/plain"
+  env.response.status = HTTP::Status::CREATED
+  "created"
+end
+
+get "/pop/:stack" do |env|
+  stack = env.params.url["stack"]
+
+  content = Kamajii.pop stack
+
+  env.response.content_type = "text/plain"
+  env.response.status = HTTP::Status::CREATED
+  content
 end
 
 error 404 do |env|
