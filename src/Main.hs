da1aa738ed3a0128fa179388bfd98a47f4083d11
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-10-12 20:02:43 -0700

    Make it a TCP server

--- src/Main.hs
+++ src/Main.hs
@@ -1,45 +1,63 @@
 module Main where
 
 import Control.Monad ( forM_ )
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack, snoc, unpack)
+import qualified Data.ByteString as B
 import Files ( getStackDir, pushItem, popItem )
+import Network.Simple.TCP
+    ( recv, send, serve, SockAddr, Socket, HostPreference(Host) )
 import System.IO (isEOF)
+import Control.Monad.IO.Class ( MonadIO )
 
-programName :: String
-programName = "kamajii 0.1"
+programName :: ByteString
+programName = pack "kamajii 0.1"
 
 programUsage :: String
 programUsage =
-  "Usage: kamajii STACK push CONTENTS..." ++
+  "Usage: kamajii STACK push CONTENTS...\n" ++
   "       kamajii STACK pop"
 
 main :: IO ()
-main = do
-  putStrLn programName
-  putStrLn $ take (length programName) (cycle "~")
-  loop
+main = serve (Host "localhost") "3000" mainSession
 
-loop :: IO ()
-loop = do
-  end <- isEOF
-  if end then
-    return ()
-  else do
-    line <- getLine
-    let command = words line
-    handle command
-    loop
+mainSession :: (Socket, SockAddr) -> IO ()
+mainSession (socket, remoteAddr) = do
+  putStrLn $ "TCP connection established from " ++ show remoteAddr
+  sendLn socket programName
+  loop socket
+  putStrLn $ "TCP connection terminated from " ++ show remoteAddr
+
+loop :: Socket -> IO ()
+loop socket = do
+  maybe_input <- recv socket 2048
+  case maybe_input of
+    Nothing -> return ()
+    Just bytes -> do
+      let input = words . unpack $ bytes
+      if input `elem` [["exit"], ["q"], ["quit"]]
+      then return ()
+      else do
+        maybe_output <- handle input
+        case maybe_output of
+          Nothing -> return ()
+          Just chars -> sendLn socket (pack chars)
+        loop socket
 
 type Commands = [String]
 type StackDir = String
 
-handle :: Commands -> IO ()
+handle :: Commands -> IO (Maybe String)
 handle (stack:cmd) = getStackDir stack >>= \dir -> stackAction dir cmd
-handle _ = putStrLn programUsage
+handle _ = return (Just programUsage)
 
-stackAction :: StackDir -> Commands -> IO ()
-stackAction stackDir ("push":contents) = pushItem stackDir (unwords contents)
-stackAction stackDir ["pop"] = popItem stackDir >>= \contents -> forM_ contents putStrLn
+stackAction :: StackDir -> Commands -> IO (Maybe String)
+stackAction stackDir ("push":contents) = pushItem stackDir (unwords contents) >> return Nothing
+stackAction stackDir ["pop"] = popItem stackDir
 -- TODO: Read actions.      peek, head <n>, list, tail, length, isempty
 -- TODO: Lifecycle actions. complete[-all] delete[-all]
 -- TODO: Shuffle actions.   swap, rot, next (move first to last)
-stackAction _ _ = putStrLn programUsage
+stackAction _ _ = return (Just programUsage)
+
+sendLn :: MonadIO m => Socket -> ByteString -> m ()
+sendLn socket bs = send socket $ snoc bs '\n'
