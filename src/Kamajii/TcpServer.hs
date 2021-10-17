76d5b9141065b6158cf9de8fb5a8c17435996bdd
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-10-17 02:17:31 -0700

    Add some log levels (come back to this later), move host/port to main

--- src/Kamajii/TcpServer.hs
+++ src/Kamajii/TcpServer.hs
@@ -18,22 +18,24 @@ import Kamajii.Stack (processStackCommand)
 programName :: ByteString
 programName = pack Meta.programName
 
-serverMain :: IO ()
-serverMain = serve (Host "localhost") "3000" handleClient
+serverMain :: String -> String -> IO ()
+serverMain host port = do
+  putStrLn $ "Serving on " ++ host ++ ":" ++ port
+  serve (Host host) port handleClient
 
 handleClient :: (Socket, SockAddr) -> IO ()
 handleClient (socket, remoteAddr) = do
-  putStrLn $ "TCP connection established from " ++ show remoteAddr
+  putStrLn $ "[INFO] TCP connection established from " ++ show remoteAddr
   sendLn socket programName
   clientLoop socket
-  putStrLn $ "TCP connection terminated from " ++ show remoteAddr
+  putStrLn $ "[INFO] TCP connection terminated from " ++ show remoteAddr
 
 clientLoop :: Socket -> IO ()
 clientLoop socket = do
   maybe_bytes <- recv socket 2048
   with maybe_bytes $ \bytes -> do
     let line = unpack bytes
-    putStrLn $ "Received command: " ++ line
+    putStrLn $ "[DEBUG] Received command: " ++ line
     let input = words line
     unless (input `elem` [["\EOT"], ["exit"], ["q"], ["quit"]]) $ do
       maybe_chars <- processStackCommand input
--- src/Main.hs
+++ src/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Kamajii.TcpServer (serverMain)
 
 main :: IO ()
-main = serverMain
+main = serverMain "localhost" "3000"
