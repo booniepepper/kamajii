cdfc7c96114e59f32790b1b0de77ad17e56aca72
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-10-30 01:30:18 -0700

    add some basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(_stream: TcpStream) {
+    //stream.write("ok bye");
+}
+
+// Starting from: https://doc.rust-lang.org/std/net/struct.TcpListener.html
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
